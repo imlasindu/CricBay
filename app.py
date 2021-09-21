d7f343f2edb1ecab48a237158def551c80386709
kothawleprem <kothawleprem@gmail.com>
2021-09-22 00:24:20 +0530

    Regular Updates

--- app.py
+++ app.py
@@ -8,38 +8,56 @@ app = Flask(__name__)
 def cricbay():
     html_text = requests.get('https://sports.ndtv.com/cricket/live-scores').text
     soup = BeautifulSoup(html_text, "html.parser")
-    sect = soup.find_all('div',class_='sp-scr_wrp')
+    sect = soup.find_all('div', class_='sp-scr_wrp vevent')
     section = sect[0]
-    description = section.find('span',class_='description').text
-    location = section.find('span',class_='location').text
-    time = section.find('div',class_='scr_dt-red').text
+    current_block = section.find('div', class_='scr_inf-wrp')
+    current = current_block.find('div', class_='scr_dt-red').text
+    teams_block = section.find_all('div', class_='scr_tm-wrp')
+    team_a = teams_block[0].text
+    team_b = teams_block[1].text
+    text_only = section.find_all('div', class_='scr_txt-ony')
+    team_a_score = 'Not Avaialable'
+    team_b_score = 'Not Avaialable'
     link = "https://sports.ndtv.com/" + section.find('a',class_='scr_ful-sbr-txt').get('href')
-    toss = ''
-    team1_name = ''
-    team1_score = ''
-    team2_name = ''
-    team2_score = ''
-    try:
-        toss = section.find('div',class_="scr_dt-red @*scr-inf_tx*@").text
-        block = section.find_all('div',class_='scr_tm-wrp')
-        team1_block = block[0]
-        team1_name = team1_block.find('div',class_='scr_tm-nm').text
-        team1_score = team1_block.find('span',class_='scr_tm-run').text
-        team2_block = block[1]
-        team2_name = team2_block.find('div',class_='scr_tm-nm').text
-        team2_score = team2_block.find('span',class_='scr_tm-run').text
-    except:
-        print("Some Error Found")
+    if 'Yet' in team_a:
+        ind = team_a.index('Yet')
+        team_a = team_a[:-(len(team_a) - ind)]
+        team_a_score = 'Yet to Bat'
+    else:
+        if 'Not Avaialable' not in team_a:
+            ind = 0
+            for j, c in enumerate(team_a):
+                if c.isdigit():
+                    ind = j
+                    break
+            team_a_score = team_a[ind:]
+            team_a = team_a[:-(len(team_a) - ind)]
+    if 'Yet' in team_b:
+        ind = team_b.index('Yet')
+        team_b = team_b[:-(len(team_b) - ind)]
+        team_b_score = 'Yet to Bat'
+    else:
+        if 'Not Available' not in team_b:
+            ind = 0
+            for j, c in enumerate(team_b):
+                if c.isdigit():
+                    ind = j
+                    break
+            team_b_score = team_b[ind:]
+            team_b = team_b[:-(len(team_b) - ind)]
+    description = text_only[0].text
+    location = text_only[-1].text
+    status = section.find('div', class_='scr-inf_lt').text
     result = {
         "Author" : "Prem",
         "Description" : description,
         "Location" : location,
-        "Toss" : toss,
-        "Status" : time,
-        "Team A" : team1_name,
-        "Team A Score" : team1_score,
-        "Team B" : team2_name,
-        "Team B Score" : team2_score,
+        "Current" : current,
+        "Status" : status,
+        "Team A" : team_a,
+        "Team A Score" : team_a_score,
+        "Team B" : team_b,
+        "Team B Score" : team_b_score,
         "Full Scoreboard" : link,
         "Thanks" : "NDTV Sports"
     }
